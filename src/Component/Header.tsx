f44fc3a2fd70b6244ce9718e8ef2d882a2b2d2bc
monique <moniquejp16@gmail.com>
2024-02-26 15:54:54 -0300

    feat: full header

---
+++ src/Component/Header.tsx
@@ -0,0 +1,14 @@
+import {All, HeaderBhutans} from "../assets/StylesPages/Header";
+import Fundo from "../assets/Styles/images/Fundo.png";
+
+export default function Header() {
+
+  return (
+    <All>
+     <HeaderBhutans>
+     <img src={Fundo} alt="" />
+     </HeaderBhutans>
+     
+    </All>
+  );
+}
---
+++ src/assets/StylesPages/Header.tsx
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const All = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+export const HeaderBhutans = styled.div`
+  width: 100vw;
+  height: 60px;
+  background-color: #ffffff;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  box-shadow: 0px 2px 8px 0px rgba(0, 0, 0, 0.2);
+  position: fixed; 
+  top: 0; 
+  left: 0; 
+  z-index: 1000;
+  img{
+    margin-left: 3%;
+  }
+`;
