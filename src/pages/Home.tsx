e0cfdeb8672655af04625a34b43b8a10caf98fd4
monique <moniquejp16@gmail.com>
2024-03-03 13:25:49 -0300

    fix: removing console.log

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,17 +6,17 @@ import ComponentThatOccupiesEmployeesTextAndSearchBar from "../component/Compone
 import TableWithTheHeaderAndOperatingDataForDesktopWebMode from "../component/TableWithTheHeaderAndOperatingDataForDesktopWebMode";
 
 export default function Home() {
-  const [list, setList] = useState<Employee[]>([]); 
+  const [list, setList] = useState<Employee[]>([]);
   const [search, setSearch] = useState("");
-  const [filter, setFilter] = useState<Employee[]>([]); 
+  const [filter, setFilter] = useState<Employee[]>([]);
 
   useEffect(() => {
     const urlRequest = `${import.meta.env.VITE_API_URL}/employees`;
 
-    axios.get<Employee[]>(urlRequest) 
+    axios
+      .get<Employee[]>(urlRequest)
       .then((response) => {
         setList(response.data);
-        console.log(response.data)
       })
       .catch((err) => {
         console.log(err.response);
@@ -24,20 +24,27 @@ export default function Home() {
   }, []);
 
   useEffect(() => {
-    const filteredList = list.filter((item) =>
-    item.name.toLowerCase().includes(search.toLowerCase()) ||
-    item.job.toLowerCase().includes(search.toLowerCase()) ||
-    item.phone.toLowerCase().includes(search.toLowerCase())
-  );
-  setFilter(filteredList);
-  
-  }, [search]); 
+    const filteredList = list.filter(
+      (item) =>
+        item.name.toLowerCase().includes(search.toLowerCase()) ||
+        item.job.toLowerCase().includes(search.toLowerCase()) ||
+        item.phone.toLowerCase().includes(search.toLowerCase())
+    );
+    setFilter(filteredList);
+  }, [search]);
 
   return (
     <TableContainer>
-      <ComponentThatOccupiesEmployeesTextAndSearchBar setSearch={setSearch} search={search} />
-      {filter.length === 0 && <TableWithTheHeaderAndOperatingDataForDesktopWebMode list={list} />}
-      {filter.length !== 0 && <TableWithTheHeaderAndOperatingDataForDesktopWebMode list={filter} />}
+      <ComponentThatOccupiesEmployeesTextAndSearchBar
+        setSearch={setSearch}
+        search={search}
+      />
+      {filter.length === 0 && (
+        <TableWithTheHeaderAndOperatingDataForDesktopWebMode list={list} />
+      )}
+      {filter.length !== 0 && (
+        <TableWithTheHeaderAndOperatingDataForDesktopWebMode list={filter} />
+      )}
     </TableContainer>
   );
 }
